452503e46aff1bd051b6c122c4894964bb88f67f
anshul4141 <anshulprajapati691@gmail.com>
2024-09-11 15:54:40 +0530

    done

--- C-Program/forloop.c
+++ C-Program/forloop.c
@@ -5,12 +5,6 @@ void main()
 
     for (int i = 1; i <= 10; i++)
     {
-
-        for (int j = 1; j <= 10; j++)
-        {
-            printf("\n%d", i * j);
-        }
-
-        printf("\n");
+        printf("Hello World %d \n", i);
     }
 }
